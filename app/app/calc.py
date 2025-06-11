9fd1d2bb54e5bfcd565be420a92b8499f2e50d8d
Ibory <lala1522@gachon.ac.kr>
2025-06-11 14:14:58 +0900

    git actions test

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,10 +2,10 @@
 Calculator functions
 """
 
-def add(x,y):
+def add(x, y):
     """Add x and y and return resualt"""
     return x + y
 
-def subtract(x,y):
+def subtract(x, y):
     """Subtract x and y return resualt"""
     return y - x
