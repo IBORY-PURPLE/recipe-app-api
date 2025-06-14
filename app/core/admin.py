b799fa51d9298d9d56c424e1be8dc64e5f4ffeaf
채우 <lala1522@gachon.ac.kr>
2025-06-14 15:32:23 +0900

    Configured user change page, add user page

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,60 @@
-from django.contrib import admin  # noqa
+"""
+django admin customizations
+"""
+from django.contrib import admin
+# 기본 useradmin클래스와 내가 커스텀하는 useradmin클래스를 구분짓기 위해서 baseuseradmin으로 import해오는 것.
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+# _('Permission')이라는 섹션 제목을 번역하고 싶을 때 번역가능하게 해주는 함수
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from core import models
+
+class UserAdmin(BaseUserAdmin):
+    """Define the admin pasges for users"""
+    ordering = ['id']
+    list_display = ['email', 'name']
+    # fieldsets 구조는 튜플의 연속으로 하나의 튜플은 user change페이지의 하나의 섹션으로 이해
+    # (섹션 제목, {'feilds': ('필드1', '필드2', '필드3')}),
+    # (
+    #      센션 제목,
+    #      {
+    #           'feild': (
+    #                 '필드1',
+    #                 '필드2',
+    #                 '필드3',
+    #            )
+    #      }
+    # ),
+    fieldsets = (
+        (None, {'fields': ('email', 'password',)}),
+        (_('Personal Info'), {'fields': ('name',)}),
+        (
+            _('Permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                )
+            }
+        ),
+        (_('Important dates'), {'fields': ('last_login',)}),
+    )
+    readonly_fields = ['last_login']
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email',
+                'password1',
+                'password2',
+                'name',
+                'is_active',
+                'is_staff',
+                'is_superuser',
+            ),
+        }),
+    )
+
+admin.site.register(models.User, UserAdmin)
+
--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -2,6 +2,8 @@
 Test for the Django modificaion
 """
 from django.test import TestCase
+# get_user_model은 User클래스 그자체 models.User라 똑같이 클래스 그자체이다.
+# setting.py에서 AUTH_USER_MODEL에 설정된 user model이 지정된다.
 from django.contrib.auth import get_user_model
 from django.urls import reverse
 from django.test import Client
@@ -13,7 +15,7 @@ class AdminSiteTests(TestCase):
     def setUp(self):
         """Create user and client"""
         self.client = Client()
-        self.admin_user = get_user_model().objects.create_user(
+        self.admin_user = get_user_model().objects.create_superuser(
             email='admin@example.com',
             password='test123',
         )
@@ -26,10 +28,24 @@ class AdminSiteTests(TestCase):
 
     def test_users_lists(self):
         """Test that users are listed on page"""
-        # core_user에 있는 userlist url을 저장하고
+        # core_useㅠr에 있는 userlist url을 저장하고
         url = reverse('admin:core_user_changelist')
         # 그 url에 있는 유저 정보를 get해와서 res변수에 저장?
         res = self.client.get(url)
 
-        self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
+        self.assertContains(res, self.user.name)
+
+    def test_edit_user_page(self):
+        """Test the edit user page works"""
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        """Test te create user page works"""
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
