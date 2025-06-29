06ffe85184b59239fce7c4ccb914a4c1965213fe
채우 <lala1522@gachon.ac.kr>
2025-06-29 18:53:27 +0900

    Refactoring Viewset tag and ingrdient duplication part

--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -16,7 +16,7 @@ router = DefaultRouter()
 # urlpatterns에 넣어서 접근을 허용해준다.
 # views.RecipeViewSet은 각 url마다 어떤 동작을 할지 정해주는 핵심 로직 클래스
 # viewset은 url와 db모델 사이의 컨트롤러 역할을 하는 클래스라고 보자.
-router.register('recipe', views.RecipeViewSet)
+router.register('recipes', views.RecipeViewSet)
 router.register('tags', views.TagViewSet)
 router.register('ingredient', views.IngredientViewSet)
 
